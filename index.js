53078fa53c83366e2fc1a31820db98abb484fef1
Wania Khan <hafizawania654@gmail.com>
2023-07-31 21:40:27 +0500

    new

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const mongoose = require('mongoose')
+const port = process.env.SERVER_PORT 
+
+app.use(express.json()) // stringify me change karnay k liye ye karyngy json ko
+app .use('/api', require('./api/users/Router'))
+
+// mongoose.connect(process.env.MONGO_URL)
+// .then(()=> console.log("DB Connected"))
+// .catch((error)=> console.log("SOmething went wrong"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
